66e439cac5f238bfa38e3181f3f364c1aceae2ff
Sam Hooke <sam@hooke.me>
2018-12-01 12:31:49 +0000

    Find first frequency that appears twice. Day 1 complete.

--- day1/solution.rs
+++ day1/solution.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashSet;
 
 fn main() -> std::io::Result<()> {
 	let mut file = File::open("input")?;
@@ -11,20 +12,43 @@ fn main() -> std::io::Result<()> {
 	let split = contents.split("\n");
 
 	let mut sum : i32 = 0;
+	let mut sums = HashSet::new();
 
-	for s in split {
-		println!("Line: {}", s);
+	sums.insert(sum);
 
-		if s.starts_with("+") {
-			println!("plus");
-		} else if s.starts_with("-") {
-			println!("minus");
+	let mut reached_twice : i32 = 0;
+	let mut found_reached_twice = false;
+
+	while found_reached_twice == false {
+		for s in split.clone() {
+			println!("Line: {}", s);
+
+			if s.starts_with("+") {
+				println!("plus");
+			} else if s.starts_with("-") {
+				println!("minus");
+			}
+
+			// XXX: Hacky way of handling unparsed lines
+			let parsed = s.parse::<i32>().unwrap_or(999999);
+			if parsed != 999999 {
+				sum += parsed;
+
+				if !found_reached_twice {
+					if sums.contains(&sum) {
+						reached_twice = sum;
+						found_reached_twice = true;
+					}
+				}
+
+				sums.insert(sum);
+			}
 		}
-
-		sum += s.parse::<i32>().unwrap_or(0);
 	}
 
 	println!("Sum: {}", sum);
+	println!("Has reached twice: {}", found_reached_twice);
+	println!("Reached twice first: {}", reached_twice);
 
 	Ok(())
 }
